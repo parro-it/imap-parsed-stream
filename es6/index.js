80a696f35d3259de870fd7030f9fa7126d72f656
Andrea Parodi <andrea@parro.it>
2015-08-11 08:29:39 +0200

    first release

--- es6/index.js
+++ es6/index.js
@@ -1,3 +1,25 @@
+import { Transform } from 'stream';
+import { MailParser } from 'mailparser';
+
+class Parser extends Transform {
+  constructor() {
+    super({ decodeStrings: true});
+  }
+
+  _transform(chunk, encoding, callback) {
+    const mail = chunk.toString('utf8');
+    const mp = new MailParser();
+    mp.on('end', mailObject => {
+      callback(null, JSON.stringify(mailObject));
+    });
+    mp.on('error', err => {
+      callback(err);
+    });
+    mp.write(mail);
+    mp.end();
+  }
+}
+
 export default async function imapParsedStream() {
-  return Promise.resolve(42);
+  return new Parser();
 }
--- test/index_test.js
+++ test/index_test.js
@@ -3,12 +3,50 @@ if (process.env.TEST_RELEASE) {
   moduleRoot = '../dist';
 }
 
+import concat from 'concat-stream';
+
+/*eslint-disable */
+const input =
+
+`Date: Sat, 28 Dec 2013 07:12:01 -0800 (PST)
+Subject: Aggiungi una foto del profilo
+From: "Google+" <noreply-bbf11161@plus.google.com>
+To: imaptest73@gmail.com
+
+
+
+Date: Tue, 03 Dec 2013 14:05:25 -0800 (PST)
+Subject: =?ISO-8859-1?Q?I_post_pi=F9_popolari_della_settimana_su_Google=2B?=
+From: "Google+" <noreply-bbf11161@plus.google.com>
+To: imaptest73@gmail.com
+
+
+
+Date: Sat, 12 Oct 2013 17:39:49 +0200
+From: "imaptest73@gmail.com" <imaptest73@gmail.com>
+Subject: this is a test
+
+
+
+`;
+/*eslint-enable */
 const imapParsedStream = require(moduleRoot);
 
-describe('imapParsedStream', () => {
+describe('imapParsedStream', done => {
   it('works', async () => {
-    const result = await imapParsedStream();
-    result.should.be.equal(42);
+    const parse = await imapParsedStream();
+
+    parse.pipe(concat({encoding: 'object'}, result => {
+      console.dir(result)
+      done();
+    }));
+
+    input.split('\n\n').forEach(m =>
+      parse.write(m)
+    );
+    parse.end();
+
+
   });
 });
 
